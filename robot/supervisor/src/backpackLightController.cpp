a8b12c7bde7dff508801d641e208f763f3ea4ae2
zyn <106452123+zynwire@users.noreply.github.com>
2025-11-10 14:00:23 +0100

    update the backpack colors
    this was made on a phone, if this has a syntax error or whatever its not my fault.

--- robot/supervisor/src/backpackLightController.cpp
+++ robot/supervisor/src/backpackLightController.cpp
@@ -115,11 +115,11 @@ namespace BackpackLightController {
           color = 0x80808000;
       } else {
         if(i == 0) {
-          color = 0x80ff0000; // red for back led
+          color = 0x80B4BEFE; // red for back led
         } else if(i == 1) {
-          color = 0x8000ff00; // green for middle led
+          color = 0x808A94D8; // green for middle led
         } else if(i == 2) {
-          color = 0x800000ff; // blue for top led
+          color = 0x80424688; // blue for top led
         } else {
           color = 0x80808000; // fallback color
         }
